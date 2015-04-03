9a7f283dc11357cc5287961b34918e37e71d00c2
Liam Marshall <archimedespi3141@gmail.com>
2015-04-03 12:41:51 -0500

    Correct order for assertEquals

--- src/test/java/ResponseCurveTest.java
+++ src/test/java/ResponseCurveTest.java
@@ -17,8 +17,8 @@ public class ResponseCurveTest {
                 new double[]{0.0, 1.0, 2.0},
                 new double[]{0.0, 1.0, 2.0});
 
-        assertEquals("respond(0.0) for y 0.0 should be 0.0", responseCurve.respond(0.0), 0.0, 0.0/*ε, a small fudge for doubles*/);
-        assertEquals("respond(1.0) for y 1.0 should be 1.0", responseCurve.respond(1.0), 1.0, 0.0/*ε, a small fudge for doubles*/);
-        assertEquals("respond(2.0) for y 2.0 should be 2.0", responseCurve.respond(2.0), 2.0, 0.0/*ε, a small fudge for doubles*/);
+        assertEquals("respond(0.0) for y 0.0 should be 0.0", 0.0, responseCurve.respond(0.0), 0.0/*ε, a small fudge for doubles*/);
+        assertEquals("respond(1.0) for y 1.0 should be 1.0", 1.0, responseCurve.respond(1.0), 0.0/*ε, a small fudge for doubles*/);
+        assertEquals("respond(2.0) for y 2.0 should be 2.0", 2.0, responseCurve.respond(2.0), 0.0/*ε, a small fudge for doubles*/);
     }
 }
