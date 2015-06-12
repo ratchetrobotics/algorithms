ebe1ad480ab0639343a20c334b11ebb24c43dbd7
Liam Marshall <liam@cpan.org>
2015-06-11 22:57:11 -0500

    Test the List<Vector2D> form of ResponseCurve

--- src/test/java/ResponseCurveTest.java
+++ src/test/java/ResponseCurveTest.java
@@ -3,8 +3,12 @@
  * Licensed under the BSD license.
  */
 
+import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
 import org.junit.Test;
 import org.ratchetrobotics.algorithms.ai.ResponseCurve;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -14,6 +18,19 @@ public class ResponseCurveTest {
         ResponseCurve responseCurve = new ResponseCurve(
                 new double[]{0.0, 1.0, 2.0},
                 new double[]{0.0, 1.0, 2.0});
+
+        assertEquals("respond(0.0) for y 0.0 should be 0.0", 0.0, responseCurve.respond(0.0), 0.0/*ε, a small fudge for doubles*/);
+        assertEquals("respond(1.0) for y 1.0 should be 1.0", 1.0, responseCurve.respond(1.0), 0.0/*ε, a small fudge for doubles*/);
+        assertEquals("respond(2.0) for y 2.0 should be 2.0", 2.0, responseCurve.respond(2.0), 0.0/*ε, a small fudge for doubles*/);
+    }
+
+    public void aVector2DShouldInterpolateToItself() {
+        List<Vector2D> points = new ArrayList<Vector2D>();
+        points.add(new Vector2D(0, 0));
+        points.add(new Vector2D(1, 1));
+        points.add(new Vector2D(2, 2));
+
+        ResponseCurve responseCurve = new ResponseCurve(points);
 
         assertEquals("respond(0.0) for y 0.0 should be 0.0", 0.0, responseCurve.respond(0.0), 0.0/*ε, a small fudge for doubles*/);
         assertEquals("respond(1.0) for y 1.0 should be 1.0", 1.0, responseCurve.respond(1.0), 0.0/*ε, a small fudge for doubles*/);
