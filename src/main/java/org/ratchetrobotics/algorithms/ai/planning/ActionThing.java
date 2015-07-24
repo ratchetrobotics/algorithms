abbb318062cb3473f76f054bb5ca73484c8e7128
Liam Marshall <archimedespi3141@gmail.com>
2015-07-23 20:03:25 -0500

    Use Vector3D instead of unimplemented custom class

--- src/main/java/org/ratchetrobotics/algorithms/ai/planning/ActionThing.java
+++ src/main/java/org/ratchetrobotics/algorithms/ai/planning/ActionThing.java
@@ -1,5 +1,7 @@
 package org.ratchetrobotics.algorithms.ai.planning;
 
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+
 /**
  * Documentation here.
  *
@@ -7,7 +9,7 @@ package org.ratchetrobotics.algorithms.ai.planning;
  * @version 1.0
  */
 public class ActionThing {
-    public ActionThing(StaticPosition position) {
+    public ActionThing(Vector3D position) {
 
     }
 }
