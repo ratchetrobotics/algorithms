57941451558c716d7730474cc88240b6a86db480
Liam Marshall <liam@cpan.org>
2015-08-03 13:30:13 -0500

    Template in FieldMap

---
+++ src/main/java/org/ratchetrobotics/algorithms/ai/mapping/FieldMap.java
@@ -0,0 +1,17 @@
+package org.ratchetrobotics.algorithms.ai.mapping;
+
+/**
+ * The FieldMap's purpose is to take data from a GeoJSON file,
+ * and present it to the AStarPathFinder in an easy-to-use way.
+ * Cool things which it will be able to do:
+ * <ul>
+ *     <li>Generate a triangulated-graph representation of any GeoJSON file!</li>
+ *     <li>Map from a triangulated-graph to actual coordinates!</li>
+ *     <li>Add pushpins for important locations!</li>
+ * </ul>
+ *
+ * @author Liam Marshall (archimedespi at Keybase)
+ * @version 1.0
+ */
+public class FieldMap {
+}
