4c5f867e9c06efd48fb0299b586a478ccde1576e
Liam Marshall <archimedespi3141@gmail.com>
2015-08-31 10:35:40 -0500

    Test that a point in GeoJSON becomes a JTS point

--- src/test/java/FieldMapTest.java
+++ src/test/java/FieldMapTest.java
@@ -3,12 +3,17 @@
  *  Licensed under the BSD license.
  */
 
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import org.geotools.feature.FeatureIterator;
 import org.junit.Test;
 import org.ratchetrobotics.algorithms.mapping.FieldMap;
 
 import java.io.IOException;
 import java.io.StringReader;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 
@@ -63,4 +68,28 @@ public class FieldMapTest {
         assertNotNull(aFieldMap.getFeatureCollection());
     }
 
+    @Test
+    public void aGeoJsonPointShouldBeTranslated() throws IOException {
+        FieldMap aFieldMap = new FieldMap("{\n" +
+                "  \"type\": \"FeatureCollection\",\n" +
+                "  \"features\": [\n" +
+                "    {\n" +
+                "      \"type\": \"Feature\",\n" +
+                "      \"geometry\": {\n" +
+                "        \"type\": \"Point\",\n" +
+                "        \"coordinates\": [\n" +
+                "          0.5,\n" +
+                "          0.5\n" +
+                "        ]\n" +
+                "      },\n" +
+                "      \"properties\": {}\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}\n");
+        FeatureIterator it = aFieldMap.getFeatureCollection().features();
+        Geometry point = (Geometry) it.next().getDefaultGeometryProperty().getValue();
+        GeometryFactory geometryFactory = new GeometryFactory();
+        Geometry expectedPoint = geometryFactory.createPoint(new Coordinate(0.5, 0.5));
+        assertEquals(point, expectedPoint);
+    }
 }
