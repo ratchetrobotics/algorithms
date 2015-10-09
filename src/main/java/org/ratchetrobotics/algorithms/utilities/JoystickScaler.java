79621cf09f26a7818f8f682d1cead92feec4e549
Liam Marshall <liam@cpan.org>
2015-10-09 16:56:56 -0500

    JoystickScaler: comment all the things!

--- src/main/java/org/ratchetrobotics/algorithms/utilities/JoystickScaler.java
+++ src/main/java/org/ratchetrobotics/algorithms/utilities/JoystickScaler.java
@@ -14,7 +14,12 @@ public class JoystickScaler {
   }
 
   public double in(double input) {
+    // there is a good reason for all the Math.abs on the next line.
+    // we clamp the input to [0, 1] from the joystick. then we clamp the output of .respond to [0, 1] as well.
+    // this is to not have to manually mirror the response curve on [-1, 0].
     double response = Math.abs(responseCurve.respond(Math.abs(input)));
+    // this line re-adds the correct sign. this makes it impossible for a screwy interpolation
+    // to run motors in the wrong direction
     return (input > 0) ? (response) : (-response);
   }
 }
