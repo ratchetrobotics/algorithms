8b0ab0dd8a63a1efcd880d97002559e3dbbfa2ef
Liam Marshall <liam@cpan.org>
2016-01-02 16:46:45 -0600

    Make JoystickScaler completely static, not requiring any initialization

--- src/main/java/org/ratchetrobotics/algorithms/utilities/JoystickScaler.java
+++ src/main/java/org/ratchetrobotics/algorithms/utilities/JoystickScaler.java
@@ -6,11 +6,10 @@ import org.ratchetrobotics.algorithms.ai.ResponseCurve;
  * Created by liam on 10/8/15.
  */
 public class JoystickScaler {
-  private ResponseCurve responseCurve;
+  private static final ResponseCurve responseCurve = new ResponseCurve(new double[]{0.1, 0.25, 0.75, 1.0},
+    new double[]{0.0, 0.3, 0.8, 1.0});
 
   public JoystickScaler() {
-    responseCurve = new ResponseCurve(new double[]{0.1, 0.25, 0.75, 1.0},
-      new double[]{0.0, 0.3, 0.8, 1});
   }
 
   public double in(double input) {
